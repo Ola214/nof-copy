c433312e1716693d760f19309e792904438ba079
aleksandra-k <aleksandra.krysinska.94@gmail.com>
2022-01-09 10:50:01 +0100

    delete unnecessary attributes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -31,7 +31,7 @@ class Menu extends Component {
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="#">KDN</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarItems" aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation" onClick={this.navButtonClicked}>
+                <button className="navbar-toggler" type="button" onClick={this.navButtonClicked}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className='collapse navbar-collapse' id="navbarItems">
