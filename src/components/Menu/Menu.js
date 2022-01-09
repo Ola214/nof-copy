b6f921a3631f7e99350603068d97fdd672dfd157
aleksandra-k <aleksandra.krysinska.94@gmail.com>
2022-01-09 17:41:39 +0100

    styling for menu added

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Menu.module.scss';
 import MenuItem from '../MenuItem/MenuItem';
+import logo from '../../images/logo.svg';
 
 class Menu extends Component {
 
@@ -19,30 +20,28 @@ class Menu extends Component {
     collapseClassModification = (showHiddenMenu) => {
         const menuItems = document.querySelector('#navbarItems');
         if(showHiddenMenu) {
-            //usuń collapse
             menuItems.classList.remove("collapse");
         } else {
-            //dodaj klasę collapse
             menuItems.classList.add("collapse");
         }
     }
 
     render() {
         return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">KDN</a>
-                <button className="navbar-toggler" type="button" onClick={this.navButtonClicked}>
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className='collapse navbar-collapse' id="navbarItems">
-                    <div className='navbar-nav'>
-                    <MenuItem link="#" value="O NAS"/>
-                    <MenuItem link="#" value="WOLONTARIAT"/>
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#"><img src={logo} alt="logo" /></a>
+                    <button className="navbar-toggler" type="button" onClick={this.navButtonClicked}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className='collapse navbar-collapse' id="navbarItems">
+                        <div className='navbar-nav'>
+                        <MenuItem link="#" value="O NAS"/>
+                        <MenuItem link="#" value="WOLONTARIAT"/>
+                        </div>
                     </div>
-                </div>
-            </nav>
-            
-
+                </nav>
+            </header>
         );
     }
 }
